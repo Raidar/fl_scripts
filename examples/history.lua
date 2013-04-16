65673a5cf630a9c6470566978c3e469b238d2c5e
Shmuel Zeigerman <solomuz0@gmail.com>
2013-04-16 17:21:44 +0300

    Make history.lua work under Far 3.

--- examples/history.lua
+++ examples/history.lua
@@ -2,79 +2,76 @@ local smenu=require"far2.searchmenu"
 
 local mFlags={ Flags = {FMENU_WRAPMODE=1}, Title='', Bottom=nil, search_plain=true }
 local F=far.Flags
-local regkey=far.PluginStartupInfo().RootKey:match('(.+)\\Plugins')
+local FarId = "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0"
 
-local function get_history(key,value)
-    local v, typ = win.GetRegKey("HKCU", key, value), ''
-    if not v then
-        far.Message( ("The is no history or error.\nRegistry message:%s"):format(typ),"History",nil,'w')
-        return
-    end
-    local st,en=1,v:find('\000')
-    local items={}
-    while st<#v do
-        local text=v:sub(st,en)
-        table.insert(items, 1, { text=text, file=text })
-        st,en=en+1,v:find('\000',en+1)
-    end
+local function get_history(subfolder)
+  local far_settings = assert(far.CreateSettings("far"))
+  local far_items = far_settings:Enum(subfolder)
+  far_settings:Free()
 
-    mFlags.SelectIndex=#items
-    return smenu(mFlags,items)
+  local items = {}
+  for _,v in ipairs(far_items) do
+    if v.PluginId == FarId then -- filter out archive plugins' items
+      table.insert(items, { text=v.Name, file=v.Name })
+    end
+  end
+
+  mFlags.SelectIndex=#items
+  return smenu(mFlags,items)
 end
 
 local function get_view_history()
-    local v,typ=far.GetRegKey('SavedViewHistory','Lines'),''
-    local t=far.GetRegKey('SavedViewHistory','Types')
-    if not v or not t then
-        far.Message( ("The is no history or error.\nRegistry message:%s"):format(typ),"History",nil,'w')
-        return
-    end
+  local far_settings = assert(far.CreateSettings("far"))
+  local view_items = far_settings:Enum(F.FSSF_HISTORY_VIEW)
+  local edit_items = far_settings:Enum(F.FSSF_HISTORY_EDIT)
+  far_settings:Free()
 
-    local st,en=1,v:find('\000')
-    local items={}
-    local i=1
-    while st<#v do
-        local text=v:sub(st,en)
-        local typ=tonumber(t:sub(i,i))
-        table.insert(items, 1, { text=string.format('%s: %s',typ==1 and 'Editor' or 'Viewer',text), file=text, typ=typ })
-        st,en=en+1,v:find('\000',en+1)
-        i=i+1
-    end
+  local items = {}
 
-    mFlags.SelectIndex=#items
-    return smenu(mFlags,items)
+  for _,v in ipairs(view_items) do
+    table.insert(items, { file=v.Name, text='Viewer│ '..v.Name, time=v.Time })
+  end
+
+  for _,v in ipairs(edit_items) do
+    table.insert(items, { file=v.Name, text='Editor│ '..v.Name, time=v.Time })
+  end
+
+  table.sort(items, function(a,b) return a.time<b.time end)
+
+  mFlags.SelectIndex=#items
+  return smenu(mFlags,items)
 end
 
-function commands_history()
-    mFlags.Title='Commands history'
-    local item,i=get_history('SavedHistory','Lines')
-    if item and item.text then
-        panel.SetCmdLine(nil,item.text)
-    end
+local function commands_history()
+  mFlags.Title='Commands history'
+  local item,i=get_history(F.FSSF_HISTORY_CMD)
+  if item and item.text then
+    panel.SetCmdLine(nil,item.text)
+  end
 end
 
-function dirs_history()
-    mFlags.Title='Folders history'
-    local item,i=get_history('SavedFolderHistory','Lines')
-    if item and item.text then
-        panel.SetPanelDirectory(nil,1,item.text)
-    end
+local function dirs_history()
+  mFlags.Title='Folders history'
+  local item,i=get_history(F.FSSF_HISTORY_FOLDER)
+  if item and item.text then
+    panel.SetPanelDirectory(nil,1,item.text)
+  end
 end
 
-function view_history()
-    mFlags.Title='View history'
-    local item,i=get_view_history()
-    if item and item.text then
-        if item.typ==1 then
-            editor.Editor(item.file,nil,nil,nil,nil,nil,F.EF_NONMODAL)
-        else
-            viewer.Viewer(item.file,nil,nil,nil,nil,nil,F.EF_NONMODAL)
-        end
+local function view_history()
+  mFlags.Title='View history'
+  local item,i=get_view_history()
+  if item and item.text then
+    if item.text:find("^Editor") then
+      editor.Editor(item.file,nil,nil,nil,nil,nil,F.EF_NONMODAL)
+    else
+      viewer.Viewer(item.file,nil,nil,nil,nil,nil,F.EF_NONMODAL)
     end
+  end
 end
 
-local call=...
-if call then
-    setfenv(loadstring(call), getfenv(1))()
-    return
+local call=(...)[1]
+if call=='commands' then commands_history()
+elseif call=='dirs' then dirs_history()
+elseif call=='view' then view_history()
 end
